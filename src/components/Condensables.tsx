0590dd0452e0664ee17546875ed9b6d94af3ac0b
eccentricdevotion <eccentricdevotion@gmail.com>
2023-09-21 21:31:03 +1200

    Make condensables pull from the GitHub source file

--- src/components/Condensables.tsx
+++ src/components/Condensables.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {parse} from "yaml";
-import axios from 'axios';
 
 function HandleRow({ymlKey, value} : {ymlKey: string, value: any}) {
     return (
@@ -17,15 +16,31 @@ export default function Condensables() {
 
     // get the condensables file and parse it
     const url = 'https://raw.githubusercontent.com/eccentricdevotion/TARDIS/master/src/main/resources/condensables.yml';
-    axios({
-        method: 'get',
-        url: url,
-        responseType: 'text'
-    })
-    .then(function (response) {
-        const data = response.data
-        const conds = parse(data)
-        // make a table
+
+    const [data, setData] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(url, {
+            method: 'GET'
+        }).then(res => res.text())
+            .then((result) => {
+                setData(result);
+                setIsLoaded(true);
+                console.log("let's retrieve the data", result);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (!isLoaded) {
         return (
             <>
                 <div className="table-responsive">
@@ -35,15 +50,34 @@ export default function Condensables() {
                                 <th>Item</th>
                                 <th>Relative value</th>
                             </tr>
-                            {Object.entries(conds).map(([key, value]) => (
-                                <HandleRow ymlKey={key} value={value} />
-                            ))}
+                            <tr>
+                                <td colSpan={2}>Grabbing data...</td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
             </>
-        );
-    });
-    return null;
+        )
+    }
+    
+    const conds = parse(data)
+    // make a table
+    return (
+        <>
+            <div className="table-responsive">
+                <table className="table table-striped table-bordered">
+                    <tbody>
+                        <tr>
+                            <th>Item</th>
+                            <th>Relative value</th>
+                        </tr>
+                        {Object.entries(conds).map(([key, value]) => (
+                            <HandleRow ymlKey={key} value={value} />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 }
 
