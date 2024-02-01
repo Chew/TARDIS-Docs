8bebaab5c86e4fad7f3d7fd8526091af18d46739
Chew <chew@chew.pw>
2024-02-01 17:55:08 -0600

    maybe remove trailing slash?

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,8 @@ const config: Config = {
   url: 'https://tardis.pages.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/`,
+  baseUrl: '',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -24,6 +25,7 @@ const config: Config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  onBrokenAnchors: 'ignore',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
