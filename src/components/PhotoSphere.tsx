3071839b9e3221d65888b3dfeb60da119982d277
Eccentric Devotion <eccentricdevotion@gmail.com>
2024-11-15 21:10:40 +1300

    Add default zoom level and caption to PhotoSphere

--- src/components/PhotoSphere.tsx
+++ src/components/PhotoSphere.tsx
@@ -5,14 +5,19 @@ import React, {useEffect} from "react";
 import "@photo-sphere-viewer/core/index.css";
 
 export function PhotoSphere({ src }: { src: string }) {
-    // generate random id
-    const id = "viewer-" + Math.random().toString(36).substring(7);
+
+    let len = src.length - 4;
+    let name = src.substring(0,len);
+    let upperFirst = name.charAt(0).toUpperCase() + name.substring(1);
+    const id = "viewer-" + name;
 
     // when the component is mounted, create a new viewer
     useEffect(() => {
         new Viewer({
             container: id,
             panorama: "/images/spheres/" + src,
+            defaultZoomLvl: 0,
+            caption: upperFirst,
         });
     });
 
